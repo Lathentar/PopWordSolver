ac1046cce3437dfb04b54dcb79a812490c693c00
Lathentar <rccorn@gmail.com>
2014-08-17 15:17:30 -0500

    Minor Improvements
    Minor improvements to code readability and speed.

--- popwordsolver.py
+++ popwordsolver.py
@@ -28,16 +28,16 @@ def parse_word_list_file(wordlistfile):
 
       return wordlist
 
-def find_words_at_pos(wordlist, board, boardSize, pos, word):
+def find_words_at_pos(wordlist, board, numRows, numColumns, pos, word):
    foundWords = []
 
-   if (pos[0] < 0 or boardSize[0] <= pos[0]):
+   if (pos[0] < 0 or numRows <= pos[0]):
       return foundWords
 
-   if (pos[1] < 0 or boardSize[1] <= pos[1]):
+   if (pos[1] < 0 or numColumns <= pos[1]):
       return foundWords
 
-   charAtPos = board[pos[1]][pos[0]]
+   charAtPos = board[pos[0]][pos[1]]
 
    # mark the letter at that position as - to indicated used
    word += charAtPos
@@ -50,45 +50,48 @@ def find_words_at_pos(wordlist, board, boardSize, pos, word):
 
    # is this a real word? add it to the found words list
    newBoard = copy.deepcopy(board)
-   newBoard[pos[1]][pos[0]] = '*'
+   newBoard[pos[0]][pos[1]] = '*'
 
    if (len(word) >= 3 and '*' in wordlist[charAtPos]):
       foundWords.append((word, newBoard))
 
    # keep searching the rest of the board with this letter removed
    if ( len(wordlist) > 1 ):
-      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, boardSize, (pos[0]-1, pos[1]-1), word)
-      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, boardSize, (pos[0]  , pos[1]-1), word)
-      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, boardSize, (pos[0]+1, pos[1]-1), word)
-      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, boardSize, (pos[0]-1, pos[1]  ), word)
-      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, boardSize, (pos[0]+1, pos[1]  ), word)
-      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, boardSize, (pos[0]-1, pos[1]+1), word)
-      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, boardSize, (pos[0]  , pos[1]+1), word)
-      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, boardSize, (pos[0]+1, pos[1]+1), word)
+      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, numRows, numColumns, (pos[0]-1, pos[1]-1), word)
+      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, numRows, numColumns, (pos[0]  , pos[1]-1), word)
+      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, numRows, numColumns, (pos[0]+1, pos[1]-1), word)
+      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, numRows, numColumns, (pos[0]-1, pos[1]  ), word)
+      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, numRows, numColumns, (pos[0]+1, pos[1]  ), word)
+      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, numRows, numColumns, (pos[0]-1, pos[1]+1), word)
+      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, numRows, numColumns, (pos[0]  , pos[1]+1), word)
+      foundWords = foundWords + find_words_at_pos(wordlist[charAtPos], newBoard, numRows, numColumns, (pos[0]+1, pos[1]+1), word)
 
    return foundWords
 
-def find_words_in_board(boardSize, board, wordlist):
+def find_words_in_board(numRows, numColumns, board, wordlist):
    foundWords = []
-   for r in xrange(0, boardSize[1]):
-      for c in xrange(0, boardSize[0]):
-         foundWords += find_words_at_pos(wordlist, board, boardSize, (c, r), '')
+   for r in xrange(0, numRows):
+      for c in xrange(0, numColumns):
+         foundWords += find_words_at_pos(wordlist, board, numRows, numColumns, (r, c), '')
 
    foundWords.sort(key=lambda x:len(x[0]), reverse=True)
    return foundWords
 
-def minimize_board(boardSize, board):
+def minimize_board(numRows, numColumns, board):
    newBoard = copy.deepcopy(board)
+   numLettersRemaining = 0
 
    # replace * with -
-   for r in xrange(0, boardSize[1]):
-      for c in xrange(0, boardSize[0]):
+   for r in xrange(0, numRows):
+      for c in xrange(0, numColumns):
          if newBoard[r][c] == '*':
             newBoard[r][c] = '-'
+         elif newBoard[r][c] != '-':
+            numLettersRemaining += 1
 
    # move letters downward
-   for r in xrange(0, boardSize[1]):
-      for c in reversed(xrange(0, boardSize[0])):
+   for r in xrange(0, numRows):
+      for c in reversed(xrange(0, numColumns)):
          if newBoard[r][c] == '-':
             # search up, until we find a valid letter
             for copyR in reversed(xrange(0, r)):
@@ -105,9 +108,9 @@ def minimize_board(boardSize, board):
 
    validColumn = 0
    #find the last valid column
-   for c in reversed(xrange(0, boardSize[0])):
+   for c in reversed(xrange(0, numColumns)):
       isValid = False
-      for r in reversed(xrange(0, boardSize[1])):
+      for r in reversed(xrange(0, numRows)):
          if newBoard[r][c] != '-':
             isValid = True
             break
@@ -120,13 +123,13 @@ def minimize_board(boardSize, board):
    while (checkColumn >= 0):
       isValid = False
       # find if the check column is empty, if so shift all columns after that over
-      for r in reversed(xrange(0, boardSize[1])):
+      for r in reversed(xrange(0, numRows)):
          if newBoard[r][checkColumn] != '-':
             isValid = True
             break
       if not isValid:
          for c in xrange(checkColumn, validColumn + 1):
-            for r in xrange(0, boardSize[1]):
+            for r in xrange(0, numRows):
                if c == validColumn:
                   newBoard[r][c] = '-'
                else:
@@ -136,11 +139,11 @@ def minimize_board(boardSize, board):
       #shift check column over by one
       checkColumn = checkColumn - 1
 
-   return newBoard
+   return ( newBoard, numLettersRemaining )
 
-def is_board_complete(boardSize, board):
-   for r in xrange(0, boardSize[1]):
-      for c in xrange(0, boardSize[0]):
+def is_board_complete(numRows, numColumns, board):
+   for r in xrange(0, numRows):
+      for c in xrange(0, numColumns):
          if board[r][c] != '-':
             return False
    return True
@@ -152,33 +155,35 @@ def board_to_string(board):
          str += c
    return str
 
-def solve_board(boardSize, board, wordlist, board_mem):
+def solve_board(numRows, numColumns, board, wordlist, board_mem):
    boardAsString = board_to_string(board)
    if boardAsString not in board_mem:
-      foundWords = find_words_in_board(boardSize, board, wordlist)
+      foundWords = find_words_in_board(numRows, numColumns, board, wordlist)
 
       board_mem[boardAsString] = None
 
       # run through the found words, minimize the board and repeat
       for wordBoardPair in foundWords:
-         newBoard = minimize_board(boardSize, wordBoardPair[1])
+         miniPair = minimize_board(numRows, numColumns, wordBoardPair[1])
+         newBoard = miniPair[0]
+         numLettersRemaining = miniPair[1]
 
-         if is_board_complete(boardSize, newBoard):
+         if numLettersRemaining == 0:
             print 'Complete', boardAsString, 'Word', wordBoardPair[0], ' WordScore', skWordScoresByLength[len(wordBoardPair[0])]
             return [ (wordBoardPair, skWordScoresByLength[len(wordBoardPair[0])]) ]
-         
-         solvedBoard = solve_board(boardSize, newBoard, wordlist, board_mem);
+         elif numLettersRemaining >= 3:         
+            solvedBoard = solve_board(numRows, numColumns, newBoard, wordlist, board_mem);
 
-         if solvedBoard != None:
-            oldScore = solvedBoard[0][1]
-            newScore = skWordScoresByLength[len(wordBoardPair[0])] + oldScore
+            if solvedBoard != None:
+               oldScore = solvedBoard[0][1]
+               newScore = skWordScoresByLength[len(wordBoardPair[0])] + oldScore
 
-            if board_mem[boardAsString] is None:
-               board_mem[boardAsString] = [(wordBoardPair, newScore)] + solvedBoard
-            else:
-               # replace if the score is higher
-               if board_mem[boardAsString][1] < newScore:
+               if board_mem[boardAsString] is None:
                   board_mem[boardAsString] = [(wordBoardPair, newScore)] + solvedBoard
+               else:
+                  # replace if the score is higher
+                  if board_mem[boardAsString][1] < newScore:
+                     board_mem[boardAsString] = [(wordBoardPair, newScore)] + solvedBoard
 
       if board_mem[boardAsString] is not None:
          print boardAsString, 'Word', board_mem[boardAsString][0][0][0], ' WordScore', skWordScoresByLength[len(board_mem[boardAsString][0][0][0])], 'TotalScore', board_mem[boardAsString][0][1]
@@ -188,21 +193,23 @@ def solve_board(boardSize, board, wordlist, board_mem):
 
 def solve_popwords(wordlist, inputfile, justFindBestWord):
    #parse the input file
-   boardSize = (0, 0)
+   numRows = 0
+   numColumns = 0
    startingBoard = []
    with open(inputfile) as f:
-      boardSize = ( int( f.readline() ), int( f.readline() ) )
+      numRows = int( f.readline() )
+      numColumns = int( f.readline() )
 
       for lines in f:
          row = lines.strip()
          startingBoard.append(list(row))
 
-         if (len(row) != boardSize[0]):
-            print 'Error: row', row, ' is not long enough, expected length', boardSize[0]
+         if (len(row) != numColumns):
+            print 'Error: row', row, ' is not long enough, expected length', numColumns
             sys.exit()
 
-      if (len(startingBoard) != boardSize[1]):
-         print 'Error: not enough rows in board', boardSize[1]
+      if (len(startingBoard) != numRows):
+         print 'Error: not enough rows in board', numRows
 
    # start with a depth first word search starting from the top left
    print 'Solving for board'
@@ -212,7 +219,7 @@ def solve_popwords(wordlist, inputfile, justFindBestWord):
       print
 
    if justFindBestWord == False:
-      solvedBoard = solve_board(boardSize, startingBoard, wordlist, {})
+      solvedBoard = solve_board(numRows, numColumns, startingBoard, wordlist, {})
       stepNum = 1
       if solvedBoard is not None:
          for step in solvedBoard:
@@ -227,7 +234,7 @@ def solve_popwords(wordlist, inputfile, justFindBestWord):
       else:
          print 'No Solution Found'
    else:
-      foundBestWord = find_words_in_board(boardSize, startingBoard, wordlist)[0]
+      foundBestWord = find_words_in_board(numRows, numColumns, startingBoard, wordlist)[0]
       print 'Best Word', foundBestWord[0]
       for line in foundBestWord[1]:
          print '   ', ''.join(line)
